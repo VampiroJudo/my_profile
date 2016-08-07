a46baf7e879ed2f6dc75eeac28347e2f6ff60a11
John <johnconley507@hotmail.com>
2016-08-07 14:34:15 -0400

    defining pages and structering home page layout

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,5 +2,21 @@ class SiteController < ApplicationController
 	def index
 		render "index"
 	end
+
+	def portfolio
+		render "portfolio"
+	end
+
+	def resume
+		render "resume"
+	end
+
+	def about
+		render "about"
+	end
+
+	def contact
+		render "contact"
+	end
 end
 
